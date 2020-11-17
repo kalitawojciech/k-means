7bcf52a4fa4256b44a187350bafc590e69f86b79
kalitawojciech <wojtek1311997@gmail.com>
2020-11-17 23:06:15 +0100

    init project, create empty form for displaying charts, working on loading data from txt file

---
+++ k-means/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace k_srednich
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chooseFileButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.kMeansButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chooseFileButton
+            // 
+            this.chooseFileButton.Location = new System.Drawing.Point(11, 46);
+            this.chooseFileButton.Name = "chooseFileButton";
+            this.chooseFileButton.Size = new System.Drawing.Size(274, 77);
+            this.chooseFileButton.TabIndex = 0;
+            this.chooseFileButton.Text = "Choose File";
+            this.chooseFileButton.UseVisualStyleBackColor = true;
+            this.chooseFileButton.Click += new System.EventHandler(this.chooseFileButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File Name";
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(86, 8);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.ReadOnly = true;
+            this.fileNameTextBox.Size = new System.Drawing.Size(200, 22);
+            this.fileNameTextBox.TabIndex = 2;
+            // 
+            // kMeansButton
+            // 
+            this.kMeansButton.Enabled = false;
+            this.kMeansButton.Location = new System.Drawing.Point(11, 140);
+            this.kMeansButton.Name = "kMeansButton";
+            this.kMeansButton.Size = new System.Drawing.Size(273, 77);
+            this.kMeansButton.TabIndex = 3;
+            this.kMeansButton.Text = "k-means";
+            this.kMeansButton.UseVisualStyleBackColor = true;
+            this.kMeansButton.Click += new System.EventHandler(this.kMeansButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 226);
+            this.Controls.Add(this.kMeansButton);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chooseFileButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button chooseFileButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Button kMeansButton;
+    }
+}
+
---
+++ k-means/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace k_srednich
+{
+    public partial class Form1 : Form
+    {
+        public string[][] uploadedData;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chooseFileButton_Click(object sender, EventArgs e)
+        {
+            string filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = @"C:\Users\wojte\Desktop";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.Filter = "Text files (*.txt)|*.txt";
+                openFileDialog.RestoreDirectory = false;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+
+
+                    string fileData = System.IO.File.ReadAllText(filePath); // wczytujemy treść pliku do zmiennej
+                    string[] rows = fileData.Trim().Split(new char[] { '\n' }); // treść pliku dzielimy wg znaku końca linii, dzięki czemu otrzymamy każdy wiersz w oddzielnej komórce tablicy
+                    string[][] uploadedData = new string[rows.Length][];   // Tworzymy zmienną, która będzie przechowywała wczytane dane. Tablica będzie miała tyle wierszy ile wierszy było z wczytanego poliku
+
+                    for (int i = 0; i < rows.Length; i++)
+                    {
+                        string row = rows[i].Trim();     // przypisuję i-ty element tablicy do zmiennej wiersz
+                        string[] cyfry = row.Split(new char[] { ' ' });   // dzielimy wiersz po znaku spacji, dzięki czemu otrzymamy tablicę cyfry, w której każda oddzielna komórka to cyfra z wiersza
+                        uploadedData[i] = new string[cyfry.Length];    // Do tablicy w której będą dane finalne dokładamy wiersz w postaci tablicy integerów tak długą jak długa jest tablica cyfry, czyli tyle ile było cyfr w jednym wierszu
+                        for (int j = 0; j < cyfry.Length; j++)
+                        {
+                            string cyfra = cyfry[j].Trim(); // przypisuję j-tą cyfrę do zmiennej cyfra
+                            uploadedData[i][j] = cyfra;
+                        }
+                    }
+
+                    this.uploadedData = uploadedData;
+                    int lastBackslashIndex = filePath.LastIndexOf('\\') + 1;
+                    this.fileNameTextBox.Text = filePath.Substring(lastBackslashIndex,  filePath.Length - lastBackslashIndex);
+                    this.kMeansButton.Enabled = true;
+
+                }
+            }
+        }
+
+        private void kMeansButton_Click(object sender, EventArgs e)
+        {
+            StartingDrawFrom startingDrawFrom = new StartingDrawFrom();
+            startingDrawFrom.Show();
+        }
+    }
+}
